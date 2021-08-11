2faab95260ccd5d23d03fb3b6d056af95fe0e334
Emanoel Delfino <emanoeldelfno@gmail.com>
2021-08-11 10:26:07 -0300

    Finish function

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ playPause.addEventListener("click", () => {
 
 function toggleText(elem, ...args) {
   const elemText = elem.innerText;
+  
+  if (args.includes(elemText) && !hasDuplicates(args)) {
+     console.log(args.indexOf(elemText));
+    if (args.indexOf(elemText) < args.length - 1) {
+      const replaceText = args[indexOf(elemText) + 1];
+    } else {
+      const replaceText = args[0];
+    }
+    console.log(replaceText);
+  } else {
+    console.log("Invalid arguments after elem. It doesn't include the text of element or it has duplicates."); 
+  }
+}
 
-  args.forEach((arg) => {
-    console.log(arg);
-  });
+function hasDuplicates(array) {
+    return (new Set(array)).size !== array.length;
 }
 
 toggleText(playPause, "a", "b", "c", "d");
