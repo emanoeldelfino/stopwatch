4ed21acb60c215666f0afe52b96e8494e752175b
emanoeldelfino <emanoeldelfno@gmail.com>
2021-08-13 02:10:41 -0300

    Add and set dark mode as default

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
 const playPause = document.querySelector("#play-pause");
+const darkModeBtn = document.querySelector("#dark-mode-btn");
+const r = document.querySelector(":root");
 
 playPause.addEventListener("click", () => {
-  const playPauseText = playPause.innerText;
-
-  // playPause.innerText = playPauseText === "play_arrow" ? "pause" : "play_arrow";
   playPause.innerText = toggleText(playPause, "play_arrow", "pause");
+});
+
+darkModeBtn.addEventListener("click", () => {
+  darkModeBtn.innerText = toggleText(darkModeBtn, "dark_mode", "lightbulb");
+  let bg, fg;
+  [bg, fg] =
+    darkModeBtn.innerText === "lightbulb"
+      ? ["black", "white"]
+      : ["white", "black"];
+  r.style.setProperty("--background", bg);
+  r.style.setProperty("--foreground", fg);
 });
 
 function toggleText(elem, ...args) {
   const elemText = elem.innerText;
-  
+
   if (args.includes(elemText) && !hasDuplicates(args)) {
     let replaceText = "";
+    console.log(args.indexOf(elemText), args.length - 1);
     if (args.indexOf(elemText) < args.length - 1) {
       replaceText = args[args.indexOf(elemText) + 1];
     } else {
       replaceText = args[0];
     }
+    console.log(replaceText);
     return replaceText;
   } else {
-    throw new Error("Invalid arguments after elem. It doesn't include the text of element or it has duplicates.");
+    throw new Error(
+      "Invalid arguments after elem. It doesn't include the text of element or it has duplicates."
+    );
   }
 }
 
 function hasDuplicates(array) {
-    return (new Set(array)).size !== array.length;
+  return new Set(array).size !== array.length;
 }
